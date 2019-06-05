416872f0da141e74e8fca9f9409fc3f57d912cff
kky <2910144663@qq.com>
2019-06-05 16:12:50 +0800

    服务中心

---
+++ PrisonService/PrisonService/Business/BusinessClasses/View/UserCenter/History/PSLocalHistoryCell.h
@@ -0,0 +1,17 @@
+//
+//  PSLocalHistoryCell.h
+//  PrisonService
+//
+//  Created by kky on 2019/6/5.
+//  Copyright © 2019年 calvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PSLocalHistoryCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PrisonService/PrisonService/Business/BusinessClasses/View/UserCenter/History/PSLocalHistoryCell.m
@@ -0,0 +1,24 @@
+//
+//  PSLocalHistoryCell.m
+//  PrisonService
+//
+//  Created by kky on 2019/6/5.
+//  Copyright © 2019年 calvin. All rights reserved.
+//
+
+#import "PSLocalHistoryCell.h"
+
+@implementation PSLocalHistoryCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
